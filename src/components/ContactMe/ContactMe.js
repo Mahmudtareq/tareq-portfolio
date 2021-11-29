e61767a1d2912b286041cda6b3606562f779794f
Mahmudtareq <tareqlpu5@gmail.com>
2021-11-29 22:23:26 +0600

    contact from

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -2,29 +2,44 @@ import Button from 'react-bootstrap/Button';
 import React from 'react';
 import { Col, Container, Form, Row } from 'react-bootstrap';
 import './ContactMe.css';
+import contact from '../../image/contact1.jpg'
 
 const ContactMe = () => {
     return (
         <div className="my-5">
             <Container className="my-5">
-                <Row>
-                    <Col xs={12} md={6} >
+                <h2 className="my-3">Contact With Me</h2>
 
-                       
+                <Row className=" g-1">
+                    <Col xs={12} md={5} >
+                       <img src={contact} alt="" />
                     </Col>
-                    <Col xs={12} md={6}>
+                    <Col className="gx-2" xs={12} md={7}>
+                        <div className=" p-3 shadow" >
                         <Form action="https://formsubmit.co/tareqlpu5@gmail.com" method="POST">
-                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                                <Form.Control type="text" placeholder="Enter your Name" required/>
-                            </Form.Group>
+                            <Row className="mb-3">
+                                <Form.Group as={Col} controlId="formGridEmail">
+                                    <Form.Control type="text" placeholder="Enter Name" />
+                                </Form.Group>
+
+                                <Form.Group as={Col} controlId="formGridPassword">
+                                    <Form.Control type="text" placeholder="Phone Number" />
+                                </Form.Group>
+                            </Row>
+                           
                             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                                 <Form.Control type="email" placeholder="Enter Your Email" required />
                             </Form.Group>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Control type="text" placeholder="Subject" required />
+                            </Form.Group>
                             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                                <Form.Control as="textarea" rows={3} placeholder="Write Message"/>
+                                <Form.Control as="textarea" rows={4} placeholder="Write Message"/>
                             </Form.Group>
                             <Button variant="primary" type="submit">Send Message</Button>
+                            <input type="hidden" name="_captcha" value="false"></input>
                         </Form>
+                        </div>
                     </Col>
                 </Row>
             </Container>
